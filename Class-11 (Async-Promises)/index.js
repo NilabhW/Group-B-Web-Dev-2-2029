d836c379ffa09a61d8ab1da13e0f334232b10b02
mrinal1224 <iammrinalkc@gmail.com>
2026-01-07 15:40:40 +0530

    push

---
+++ Class-11 (Async-Promises)/index.js
@@ -0,0 +1,33 @@
+// pending -> Fulfilled -> Rejected -> Settles
+
+// coin toss - heads - Promise Success
+//tails - promise failure
+
+const promise1 = new Promise(function (resolve, reject) {
+  const isHeads = Math.random() >= 0.5;
+
+  if (isHeads) {
+    resolve("Promise Successfull we got heads");
+  } else {
+    reject("promise unsuccessfull we got tails");
+  }
+});
+
+// then and catch
+promise1.then(function (result) {
+  console.log(result);
+});
+
+// error handling
+
+promise1.catch(function (result) {
+  console.log(result);
+});
+
+// settle
+
+promise1.finally(function(){
+    console.log('Coin Toss Done')
+})
+
+

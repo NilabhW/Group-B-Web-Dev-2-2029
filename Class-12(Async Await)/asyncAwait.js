91c6ce55e790bc29fce404523b21b893f328c84d
mrinal1224 <iammrinalkc@gmail.com>
2026-01-12 17:01:40 +0530

    async and await

--- Class-12(Async Await)/asyncAwait.js
+++ Class-12(Async Await)/asyncAwait.js
@@ -1,25 +1,23 @@
 // async await is a way to handle promises in a better way when
 // we need to perform sequinetial operations
 
+const p1 = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve("Promise 1 Resolved");
+  }, 40000);
+});
 
+const p2 = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve("Promise 2 Resolved");
+  }, 60000);
+});
 
-const p1 = new Promise(function(resolve , reject){
-    setTimeout(function(){
-      resolve('Promise 1 Resolved')
-    })
-})
-
-const p2 = new Promise(function(resolve , reject){
-    setTimeout(function(){
-      resolve('Promise 2 Resolved')
-    })
-})
-
-const p3 = new Promise(function(resolve , reject){
-    setTimeout(function(){
-      resolve('Promise 3 Resolved')
-    })
-})
+const p3 = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve("Promise 3 Resolved");
+  }, 70000);
+});
 
 //resolve this promise
 
@@ -27,13 +25,25 @@ const p3 = new Promise(function(resolve , reject){
 //     console.log(result)
 // })
 
-async function greet(){
-       let result1 = await p1
-       let result2 = await p2
-       let result3 = await p3
-       console.log(result1)
-       console.log(result2)
-       console.log(result3)
+async function greet() {
+  console.log("Scaler");
+  let result1 = await p1; // suspenion
+
+  console.log("Hello");
+  let result2 = await p2; 
+
+  console.log("World");
+
+  let result3 = await p3; 
+
+  console.log(result1);
+  console.log(result2)
+  console.log(result3)
+} // Synchornously
+
+function sayBye() {
+  console.log("Byeee");
 }
 
-greet()
+greet();
+sayBye();
--- Class-12(Async Await)/promiseChain.js
+++ Class-12(Async Await)/promiseChain.js
@@ -22,24 +22,40 @@ function generateBill() {
 
 //   promise chaining
 
-placeOrder("coffee")
-  .then(function (order) {
-    console.log(order); // Order taken for coffee
-    return order;
-  })
-  .then(function (placedOrder) {
-    let orderData = processOrder(placedOrder);
+// placeOrder("coffee")
+//   .then(function (order) {
+//     console.log(order); // Order taken for coffee
+//     return order;
+//   })
+//   .then(function (placedOrder) {
+//     let orderData = processOrder(placedOrder);
 
-    return orderData;
-  })
-  .then(function (orderData) {
-    console.log(orderData);
-    let billdata = generateBill();
-    return billdata;
-  })
-  .then(function (billData) {
-    console.log(billData);
-  })
-  .catch(function (err) {
-    console.log(err);
-  });
+//     return orderData;
+//   })
+//   .then(function (orderData) {
+//     console.log(orderData);
+//     let billdata = generateBill();
+//     return billdata;
+//   })
+//   .then(function (billData) {
+//     console.log(billData);
+//   })
+//   .catch(function (err) {
+//     console.log(err);
+//   });
+
+async function serveOrder() {
+  try {
+    const orderPlaced = await placeOrder("coffee");
+    const placedOrder = await processOrder(orderPlaced);
+    const finalBill = await generateBill();
+
+    console.log(orderPlaced);
+    console.log(placedOrder);
+    console.log(finalBill);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+serveOrder();
